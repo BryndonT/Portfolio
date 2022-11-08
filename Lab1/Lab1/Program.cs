6141f3efe3e881d618273d42fc3384e8ddfa4189
BryndonT <117744839+BryndonT@users.noreply.github.com>
2022-11-08 08:54:55 -0500

    Add files via upload
    C# Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Grading ID: S1908, Lab 1, Due 01/24/2021, CIS 199-02, this program prints out a students' Grading ID, hobbies,
+   favorite book, and favorite movie */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Grading ID:      S1908");
+            WriteLine("Hobbies:         Skateboarding and watching movies");
+            WriteLine("Favorite Book:   Cirque Du Freak");
+            WriteLine("Favorite Movie:  Interstellar");
+        }
+    }
+}
