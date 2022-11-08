08471c27eb8571028ad70fddeab35677e5bb2944
BryndonT <117744839+BryndonT@users.noreply.github.com>
2022-11-08 09:04:02 -0500

    Add files via upload
    C# Program1

---
+++ Program 1/Program 1/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Program_1
+{
+    partial class Program1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblWidthOfGarden = new System.Windows.Forms.Label();
+            this.lblLengthOfGarden = new System.Windows.Forms.Label();
+            this.lblSoilPrice = new System.Windows.Forms.Label();
+            this.lblFertilizer = new System.Windows.Forms.Label();
+            this.lblYesNoStatement1 = new System.Windows.Forms.Label();
+            this.lblFirstGarden = new System.Windows.Forms.Label();
+            this.lblYesNoStatement2 = new System.Windows.Forms.Label();
+            this.txtWidthOfGardenOutput = new System.Windows.Forms.TextBox();
+            this.txtLengthOfGardenOutput = new System.Windows.Forms.TextBox();
+            this.txtSoilPriceOutput = new System.Windows.Forms.TextBox();
+            this.txtFertilizerOutput = new System.Windows.Forms.TextBox();
+            this.txtFirstGardenOutput = new System.Windows.Forms.TextBox();
+            this.lblSqYards = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFertilizerCost = new System.Windows.Forms.Label();
+            this.lblLaborCost = new System.Windows.Forms.Label();
+            this.lblTotalCost = new System.Windows.Forms.Label();
+            this.lblSqYardsOutput = new System.Windows.Forms.Label();
+            this.lblSoilCostOutput = new System.Windows.Forms.Label();
+            this.lblFertilizerCostOutput = new System.Windows.Forms.Label();
+            this.lblLaborCostOutput = new System.Windows.Forms.Label();
+            this.lblTotalCostOutput = new System.Windows.Forms.Label();
+            this.btnCalculateEstimate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Location = new System.Drawing.Point(74, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(227, 15);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "EZ-Garden and Landscaping Costs Estimator";
+            // 
+            // lblWidthOfGarden
+            // 
+            this.lblWidthOfGarden.Location = new System.Drawing.Point(34, 44);
+            this.lblWidthOfGarden.Name = "lblWidthOfGarden";
+            this.lblWidthOfGarden.Size = new System.Drawing.Size(132, 23);
+            this.lblWidthOfGarden.TabIndex = 1;
+            this.lblWidthOfGarden.Text = "Max Width of Garden (ft):";
+            this.lblWidthOfGarden.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLengthOfGarden
+            // 
+            this.lblLengthOfGarden.AutoSize = true;
+            this.lblLengthOfGarden.Location = new System.Drawing.Point(35, 79);
+            this.lblLengthOfGarden.Name = "lblLengthOfGarden";
+            this.lblLengthOfGarden.Size = new System.Drawing.Size(131, 13);
+            this.lblLengthOfGarden.TabIndex = 2;
+            this.lblLengthOfGarden.Text = "Max Length of Garden (ft):";
+            this.lblLengthOfGarden.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSoilPrice
+            // 
+            this.lblSoilPrice.Location = new System.Drawing.Point(66, 105);
+            this.lblSoilPrice.Name = "lblSoilPrice";
+            this.lblSoilPrice.Size = new System.Drawing.Size(100, 23);
+            this.lblSoilPrice.TabIndex = 3;
+            this.lblSoilPrice.Text = "Soil Price (sq. yd):";
+            this.lblSoilPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFertilizer
+            // 
+            this.lblFertilizer.Location = new System.Drawing.Point(116, 128);
+            this.lblFertilizer.Name = "lblFertilizer";
+            this.lblFertilizer.Size = new System.Drawing.Size(50, 27);
+            this.lblFertilizer.TabIndex = 4;
+            this.lblFertilizer.Text = "Fertilizer:";
+            this.lblFertilizer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblYesNoStatement1
+            // 
+            this.lblYesNoStatement1.Location = new System.Drawing.Point(66, 155);
+            this.lblYesNoStatement1.Name = "lblYesNoStatement1";
+            this.lblYesNoStatement1.Size = new System.Drawing.Size(100, 23);
+            this.lblYesNoStatement1.TabIndex = 5;
+            this.lblYesNoStatement1.Text = "(1 = YES, 0 = NO)";
+            this.lblYesNoStatement1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFirstGarden
+            // 
+            this.lblFirstGarden.AutoSize = true;
+            this.lblFirstGarden.Location = new System.Drawing.Point(99, 185);
+            this.lblFirstGarden.Name = "lblFirstGarden";
+            this.lblFirstGarden.Size = new System.Drawing.Size(67, 13);
+            this.lblFirstGarden.TabIndex = 6;
+            this.lblFirstGarden.Text = "First Garden:";
+            this.lblFirstGarden.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblYesNoStatement2
+            // 
+            this.lblYesNoStatement2.AutoSize = true;
+            this.lblYesNoStatement2.Location = new System.Drawing.Point(74, 211);
+            this.lblYesNoStatement2.Name = "lblYesNoStatement2";
+            this.lblYesNoStatement2.Size = new System.Drawing.Size(92, 13);
+            this.lblYesNoStatement2.TabIndex = 7;
+            this.lblYesNoStatement2.Text = "(1 = YES, 0 = NO)";
+            this.lblYesNoStatement2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtWidthOfGardenOutput
+            // 
+            this.txtWidthOfGardenOutput.Location = new System.Drawing.Point(166, 47);
+            this.txtWidthOfGardenOutput.Name = "txtWidthOfGardenOutput";
+            this.txtWidthOfGardenOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtWidthOfGardenOutput.TabIndex = 8;
+            // 
+            // txtLengthOfGardenOutput
+            // 
+            this.txtLengthOfGardenOutput.Location = new System.Drawing.Point(166, 76);
+            this.txtLengthOfGardenOutput.Name = "txtLengthOfGardenOutput";
+            this.txtLengthOfGardenOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtLengthOfGardenOutput.TabIndex = 9;
+            // 
+            // txtSoilPriceOutput
+            // 
+            this.txtSoilPriceOutput.Location = new System.Drawing.Point(166, 107);
+            this.txtSoilPriceOutput.Name = "txtSoilPriceOutput";
+            this.txtSoilPriceOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtSoilPriceOutput.TabIndex = 10;
+            // 
+            // txtFertilizerOutput
+            // 
+            this.txtFertilizerOutput.Location = new System.Drawing.Point(166, 132);
+            this.txtFertilizerOutput.Name = "txtFertilizerOutput";
+            this.txtFertilizerOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtFertilizerOutput.TabIndex = 11;
+            // 
+            // txtFirstGardenOutput
+            // 
+            this.txtFirstGardenOutput.Location = new System.Drawing.Point(166, 181);
+            this.txtFirstGardenOutput.Name = "txtFirstGardenOutput";
+            this.txtFirstGardenOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstGardenOutput.TabIndex = 12;
+            // 
+            // lblSqYards
+            // 
+            this.lblSqYards.AutoSize = true;
+            this.lblSqYards.Location = new System.Drawing.Point(110, 255);
+            this.lblSqYards.Name = "lblSqYards";
+            this.lblSqYards.Size = new System.Drawing.Size(56, 13);
+            this.lblSqYards.TabIndex = 13;
+            this.lblSqYards.Text = "Sq. Yards:";
+            this.lblSqYards.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Soil Cost:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFertilizerCost
+            // 
+            this.lblFertilizerCost.AutoSize = true;
+            this.lblFertilizerCost.Location = new System.Drawing.Point(94, 321);
+            this.lblFertilizerCost.Name = "lblFertilizerCost";
+            this.lblFertilizerCost.Size = new System.Drawing.Size(72, 13);
+            this.lblFertilizerCost.TabIndex = 15;
+            this.lblFertilizerCost.Text = "Fertilizer Cost:";
+            this.lblFertilizerCost.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLaborCost
+            // 
+            this.lblLaborCost.AutoSize = true;
+            this.lblLaborCost.Location = new System.Drawing.Point(105, 355);
+            this.lblLaborCost.Name = "lblLaborCost";
+            this.lblLaborCost.Size = new System.Drawing.Size(61, 13);
+            this.lblLaborCost.TabIndex = 16;
+            this.lblLaborCost.Text = "Labor Cost:";
+            this.lblLaborCost.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTotalCost
+            // 
+            this.lblTotalCost.AutoSize = true;
+            this.lblTotalCost.Location = new System.Drawing.Point(108, 385);
+            this.lblTotalCost.Name = "lblTotalCost";
+            this.lblTotalCost.Size = new System.Drawing.Size(58, 13);
+            this.lblTotalCost.TabIndex = 17;
+            this.lblTotalCost.Text = "Total Cost:";
+            this.lblTotalCost.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSqYardsOutput
+            // 
+            this.lblSqYardsOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSqYardsOutput.Location = new System.Drawing.Point(166, 250);
+            this.lblSqYardsOutput.Name = "lblSqYardsOutput";
+            this.lblSqYardsOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblSqYardsOutput.TabIndex = 18;
+            this.lblSqYardsOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSoilCostOutput
+            // 
+            this.lblSoilCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSoilCostOutput.Location = new System.Drawing.Point(166, 284);
+            this.lblSoilCostOutput.Name = "lblSoilCostOutput";
+            this.lblSoilCostOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblSoilCostOutput.TabIndex = 19;
+            this.lblSoilCostOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblFertilizerCostOutput
+            // 
+            this.lblFertilizerCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFertilizerCostOutput.Location = new System.Drawing.Point(166, 316);
+            this.lblFertilizerCostOutput.Name = "lblFertilizerCostOutput";
+            this.lblFertilizerCostOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblFertilizerCostOutput.TabIndex = 20;
+            this.lblFertilizerCostOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblLaborCostOutput
+            // 
+            this.lblLaborCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLaborCostOutput.Location = new System.Drawing.Point(166, 350);
+            this.lblLaborCostOutput.Name = "lblLaborCostOutput";
+            this.lblLaborCostOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblLaborCostOutput.TabIndex = 21;
+            this.lblLaborCostOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTotalCostOutput
+            // 
+            this.lblTotalCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalCostOutput.Location = new System.Drawing.Point(166, 380);
+            this.lblTotalCostOutput.Name = "lblTotalCostOutput";
+            this.lblTotalCostOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblTotalCostOutput.TabIndex = 22;
+            this.lblTotalCostOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnCalculateEstimate
+            // 
+            this.btnCalculateEstimate.Location = new System.Drawing.Point(119, 429);
+            this.btnCalculateEstimate.Name = "btnCalculateEstimate";
+            this.btnCalculateEstimate.Size = new System.Drawing.Size(132, 23);
+            this.btnCalculateEstimate.TabIndex = 23;
+            this.btnCalculateEstimate.Text = "Calculate Estimate";
+            this.btnCalculateEstimate.UseVisualStyleBackColor = true;
+            this.btnCalculateEstimate.Click += new System.EventHandler(this.btnCalculateEstimate_Click);
+            // 
+            // Program1
+            // 
+            this.AcceptButton = this.btnCalculateEstimate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 480);
+            this.Controls.Add(this.btnCalculateEstimate);
+            this.Controls.Add(this.lblTotalCostOutput);
+            this.Controls.Add(this.lblLaborCostOutput);
+            this.Controls.Add(this.lblFertilizerCostOutput);
+            this.Controls.Add(this.lblSoilCostOutput);
+            this.Controls.Add(this.lblSqYardsOutput);
+            this.Controls.Add(this.lblTotalCost);
+            this.Controls.Add(this.lblLaborCost);
+            this.Controls.Add(this.lblFertilizerCost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSqYards);
+            this.Controls.Add(this.txtFirstGardenOutput);
+            this.Controls.Add(this.txtFertilizerOutput);
+            this.Controls.Add(this.txtSoilPriceOutput);
+            this.Controls.Add(this.txtLengthOfGardenOutput);
+            this.Controls.Add(this.txtWidthOfGardenOutput);
+            this.Controls.Add(this.lblYesNoStatement2);
+            this.Controls.Add(this.lblFirstGarden);
+            this.Controls.Add(this.lblYesNoStatement1);
+            this.Controls.Add(this.lblFertilizer);
+            this.Controls.Add(this.lblSoilPrice);
+            this.Controls.Add(this.lblLengthOfGarden);
+            this.Controls.Add(this.lblWidthOfGarden);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Program1";
+            this.Text = "Program 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblWidthOfGarden;
+        private System.Windows.Forms.Label lblLengthOfGarden;
+        private System.Windows.Forms.Label lblSoilPrice;
+        private System.Windows.Forms.Label lblFertilizer;
+        private System.Windows.Forms.Label lblYesNoStatement1;
+        private System.Windows.Forms.Label lblFirstGarden;
+        private System.Windows.Forms.Label lblYesNoStatement2;
+        private System.Windows.Forms.TextBox txtWidthOfGardenOutput;
+        private System.Windows.Forms.TextBox txtLengthOfGardenOutput;
+        private System.Windows.Forms.TextBox txtSoilPriceOutput;
+        private System.Windows.Forms.TextBox txtFertilizerOutput;
+        private System.Windows.Forms.TextBox txtFirstGardenOutput;
+        private System.Windows.Forms.Label lblSqYards;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFertilizerCost;
+        private System.Windows.Forms.Label lblLaborCost;
+        private System.Windows.Forms.Label lblTotalCost;
+        private System.Windows.Forms.Label lblSqYardsOutput;
+        private System.Windows.Forms.Label lblSoilCostOutput;
+        private System.Windows.Forms.Label lblFertilizerCostOutput;
+        private System.Windows.Forms.Label lblLaborCostOutput;
+        private System.Windows.Forms.Label lblTotalCostOutput;
+        private System.Windows.Forms.Button btnCalculateEstimate;
+    }
+}
+
---
+++ Program 1/Program 1/Form1.cs
@@ -0,0 +1,73 @@
+﻿// Grading ID: S1908
+// Program Number: 1
+// Due Date: 02/16/2021
+// Course Section: CIS 199-02 
+// This program requires the user to fill in textboxes with information regarding their garden/land and converts it to indivudal costs and a total cost estimate.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class Program1 : Form
+    {
+        public Program1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateEstimate_Click(object sender, EventArgs e)
+        {
+            const double AddLaborCost = 50; // Lines 31-35 declare constants for rates and a conversion to sq yards.
+            const double FertilizerRate = 4.25; 
+            const double ExtraFertilizer = 1.1;
+            const double LaborRate = 3.25;
+            const double SqYardsConversion = 9;
+            double GardenLengthInput; // Lines 36-40 declares the variables used for gather user input data from textboxes.
+            double GardenWidthInput;
+            double SoilPriceInput;
+            double FertilizerInput;
+            double FirstGardenInput;
+            double SquareYards; // Lines 41-45 declares the variables that are used when calculating the relative equations.
+            double SoilCost;
+            double FertilizerCost;
+            double LaborCost;
+            double TotalCost;
+
+            GardenLengthInput = double.Parse(txtWidthOfGardenOutput.Text); // Lines 47-51 takes user information from the textboxes and puts them into variables.
+            GardenWidthInput = double.Parse(txtLengthOfGardenOutput.Text);
+            SoilPriceInput = double.Parse(txtSoilPriceOutput.Text);
+            FertilizerInput = double.Parse(txtFertilizerOutput.Text);
+            FirstGardenInput = double.Parse(txtFirstGardenOutput.Text);
+
+            SquareYards = (GardenLengthInput * GardenWidthInput) / SqYardsConversion; // Calculates square yards
+
+            SoilCost = (SquareYards * SoilPriceInput) * ExtraFertilizer; // Calculates soil cost
+
+            FertilizerCost = SquareYards * FertilizerRate; // Caluclates fertilizer cost. 1 for being included and 0 for not being included in the total cost.
+            if (FertilizerInput < 1)
+                FertilizerCost = 0;
+
+            LaborCost = SquareYards * LaborRate; // Calculates labor cost. If a first garden has a 1, an extra 50 is charged to the labor cost.
+            if (FirstGardenInput > 0)
+                LaborCost += AddLaborCost;
+
+            TotalCost = SoilCost + FertilizerCost + LaborCost; // Calculates the total cost by adding the three individual costs.
+
+            lblSqYardsOutput.Text = $"{SquareYards:F1}"; // Lines 68-72 output the calculated estimates onto the correct labels.
+            lblSoilCostOutput.Text = $"{SoilCost:C2}";
+            lblFertilizerCostOutput.Text = $"{FertilizerCost:C2}";
+            lblLaborCostOutput.Text = $"{LaborCost:C2}";
+            lblTotalCostOutput.Text = $"{TotalCost:C2}";
+
+
+        }
+    }
+}
